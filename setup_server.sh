a4e59298a334485ed5142e70bb6bc07f8c0789d8
Oscar Eg Gensmann <oscar.gensmann@ubivox.com>
2020-04-21 16:21:39 +0000

    fix

--- setup_server.sh
+++ setup_server.sh
@@ -22,7 +22,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt install -y docker-ce
-systemctl status docker
 
 # Install docker composer
 curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
